ba8cd4449b593f5862bf676e9bd745c036700a66
fatih-berktug <faatihberktug@hotmail.com>
2022-03-05 11:37:11 +0300

    commit inital

---
+++ src/FatihBerktug.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FatihBerktug {
+	//12345678 -> 87654321
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bir Sayi Giriniz:");
+		int number =  scanner.nextInt();
+		System.out.println(reverse(number));
+	}
+	
+	public static int reverse(int number) {
+		int result=0;
+		//12345678 1234567
+		while(number>0) {
+			result=(10*result)+(number%10);
+			number/=10;
+		}
+		return result;
+	}
+
+}
