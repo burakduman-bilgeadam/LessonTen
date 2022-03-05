9175528b9c1fba05ca3763c1c1190790a8a02b0b
snhynz <snhynz@gmail.com>
2022-03-05 11:13:31 +0300

    İbrahim Senih

---
+++ src/İbrahimSenih.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class İbrahimSenih {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bir Sayi Giriniz:");
+		int number =  scanner.nextInt();
+		System.out.println(reverse(number));
+	}
+	
+	public static int reverse(int number) {
+		int result=0;
+		//12345678 1234567
+		while(number>0) {
+			result=(10*result)+(number%10);
+			number/=10;
+		}
+		return result;
+
+	}
+
+}
